e1835c4111f400e973f65c474c10e00b2fd60523
Kartik ATR923 <kartik.saini@antiersolutions.com>
2025-08-07 10:16:59 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -18,6 +18,7 @@ export const createTables = async () => {
           id UUID DEFAULT gen_random_uuid(),
           name TEXT NOT NULL,
           email TEXT UNIQUE NOT NULL PRIMARY KEY,
+          password TEXT NOT NULL,
           role TEXT,
           organisation_name TEXT,
           contract_number TEXT,
